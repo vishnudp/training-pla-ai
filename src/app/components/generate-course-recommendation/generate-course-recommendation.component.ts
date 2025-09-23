cd18517e1d2c638f5d6ba613aa36fa7c961f5f41
Vishnu Bansal <vishnubansalcomp@gmail.com>
2025-09-23 15:47:04 +0530

    popup open issue

--- src/app/components/generate-course-recommendation/generate-course-recommendation.component.ts
+++ src/app/components/generate-course-recommendation/generate-course-recommendation.component.ts
@@ -400,13 +400,13 @@ export class GenerateCourseRecommendationComponent {
   }
 
   suggestMoreCourses() {
-    this.dialogRef.close()
+    // this.dialogRef.close()
     const dialogRefNew = this.dialog.open(SuggestMoreCoursesComponent, {
       width: '1000px',
       data: { recommended_course_id: this.recommended_course_id, role_mapping_id: this.planData.id },
       panelClass: 'view-cbp-plan-popup',
-      minHeight: '300px',          // Set minimum height
-      maxHeight: '80vh',           // Prevent it from going beyond viewport
+      minHeight: '400px',          // Set minimum height
+      maxHeight: '90vh',           // Prevent it from going beyond viewport
       disableClose: true // Optional: prevent closing with outside click
     });
 
@@ -414,6 +414,7 @@ export class GenerateCourseRecommendationComponent {
       if (result === 'saved') {
         console.log('Changes saved!');
         // Refresh data or show a toast here
+        this.generateCourseRecommendation(this.planData)
       }  else {
         this.generateCourseRecommendation(this.planData)
       }
